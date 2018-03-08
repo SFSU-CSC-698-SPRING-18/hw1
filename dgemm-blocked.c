70ad04c7580dfee2df3c46132ed50874ede293da
Prateek Gupta <guptaprateek328@gmail.com>
2018-03-08 12:12:51 -0800

    cahnegd block sixe to 64

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -17,7 +17,7 @@ LDLIBS = -lrt -Wl,--start-group $(MKLROOT)/lib/intel64/libmkl_intel_lp64.a $(MKL
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 64
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
