2fe23ef2f56f3c278b7f02a2de825fed585d4a88
Prateek Gupta <guptaprateek328@gmail.com>
2018-03-08 12:38:40 -0800

    Added missing {} in for loop

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -126,36 +126,37 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
  * On exit, A and B maintain their input values. */  
-    void square_dgemm (int lda, double* A, double* B, double* C)
-    {
+  void square_dgemm (int lda, double* A, double* B, double* C)
+  {
   /* For each block-row of A */ 
-      for (int i = 0; i < lda; i += BLOCK_SIZE)
+    for (int i = 0; i < lda; i += BLOCK_SIZE)
     /* For each block-column of B */
-        for (int j = 0; j < lda; j += BLOCK_SIZE)
+      for (int j = 0; j < lda; j += BLOCK_SIZE){
 
-          int mul_j = j * lda;
-      
+        int mul_j = j * lda;
+        
       /* Accumulate block dgemms into block of C */
-          for (int k = 0; k < lda; k += BLOCK_SIZE)
-          {
+        for (int k = 0; k < lda; k += BLOCK_SIZE)
+        {
 	/* Correct block dimensions if block "goes off edge of" the matrix */
-           int M = min (BLOCK_SIZE, lda-i);
-           int N = min (BLOCK_SIZE, lda-j);
-           int K = min (BLOCK_SIZE, lda-k);
+         int M = min (BLOCK_SIZE, lda-i);
+         int N = min (BLOCK_SIZE, lda-j);
+         int K = min (BLOCK_SIZE, lda-k);
 
-           int mul_k = k * lda;
-           
-           double* res_A = A + (i + mul_k);
-           double* res_C = C + (i + mul_j);
-           double* res_B = B + (k + mul_j);
+         int mul_k = k * lda;
+         
+         double* res_A = A + (i + mul_k);
+         double* res_C = C + (i + mul_j);
+         double* res_B = B + (k + mul_j);
 
 	/* Perform individual block dgemm */
-           if((M == BLOCK_SIZE) && (N == BLOCK_SIZE) && (K == BLOCK_SIZE)){
+         if((M == BLOCK_SIZE) && (N == BLOCK_SIZE) && (K == BLOCK_SIZE)){
             //do_block_fast(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
-            do_block_fast(lda, M, N, K, res_A, res_B, res_C);
-          }else{
+          do_block_fast(lda, M, N, K, res_A, res_B, res_C);
+        }else{
     /* Perform individual block dgemm */
-            do_block(lda, M, N, K, res_A, res_B, res_C);
-          }
+          do_block(lda, M, N, K, res_A, res_B, res_C);
         }
       }
+    }
+  }
